00e69900627964cb023d042bf0aa05a546fa91d1
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2025-02-08 09:00:00 +0900

    ci:  use biome instead of eslint and prettier

--- my-app/components/Collapsible.tsx
+++ my-app/components/Collapsible.tsx
@@ -1,18 +1,18 @@
-import { PropsWithChildren, useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { type PropsWithChildren, useState } from 'react'
+import { StyleSheet, TouchableOpacity } from 'react-native'
 
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { IconSymbol } from '@/components/ui/IconSymbol'
+import { Colors } from '@/constants/Colors'
+import { useColorScheme } from '@/hooks/useColorScheme'
 
 export function Collapsible({
   children,
   title,
 }: PropsWithChildren & { title: string }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const theme = useColorScheme() ?? "light";
+  const [isOpen, setIsOpen] = useState(false)
+  const theme = useColorScheme() ?? 'light'
 
   return (
     <ThemedView>
@@ -25,25 +25,25 @@ export function Collapsible({
           name="chevron.right"
           size={18}
           weight="medium"
-          color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
-          style={{ transform: [{ rotate: isOpen ? "90deg" : "0deg" }] }}
+          color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
+          style={{ transform: [{ rotate: isOpen ? '90deg' : '0deg' }] }}
         />
 
         <ThemedText type="defaultSemiBold">{title}</ThemedText>
       </TouchableOpacity>
       {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
     </ThemedView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   heading: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 6,
   },
   content: {
     marginTop: 6,
     marginLeft: 24,
   },
-});
+})
